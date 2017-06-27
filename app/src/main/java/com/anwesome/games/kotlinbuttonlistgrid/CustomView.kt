581fdd33b1f1ee0c0142e152717b07ef0d3a1068
Anwesh43 <anweshthecool0@gmail.com>
2017-06-27 22:54:09 +0530

    adding Button class

--- app/src/main/java/com/anwesome/games/kotlinbuttonlistgrid/CustomView.kt
+++ app/src/main/java/com/anwesome/games/kotlinbuttonlistgrid/CustomView.kt
@@ -2,6 +2,7 @@ package com.anwesome.games.kotlinbuttonlistgrid
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.view.MotionEvent
 import android.view.View
@@ -9,6 +10,7 @@ import android.view.View
 /**
  * Created by anweshmishra on 27/06/17.
  */
+val paint:Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 class CustomView(ctx:Context):View(ctx) {
     override fun onDraw(canvas:Canvas) {
     }
@@ -21,11 +23,80 @@ class CustomView(ctx:Context):View(ctx) {
         return true
     }
 }
-data class ButtonGrid(val n:Int) {
-    fun draw(canvas:Context,paing:Paint) {
-
+class ButtonGrid {
+    var btns:ArrayList<Button> = ArrayList<Button>()
+    constructor(w:Float,h:Float) {
+        init(w,h,9)
+    }
+    fun init(w:Float,h:Float,n:Int) {
+        for(i in 0..9) {
+            btns.add(Button(w,h,i))
+        }
+    }
+    fun draw(canvas:Canvas,paing:Paint) {
+        btns.forEach {
+            it.draw(canvas)
+        }
     }
     fun handleTap(x:Float,y:Float):Boolean {
         return true
+    }
+}
+class Button {
+    var x:Float = 0.0f
+    var y:Float = 0.0f
+    var size:Float = 0.0f
+    var scale:Float = 0.0f
+    var dir:Float = 0.0f
+    var i:Int = 0
+    constructor(w:Float,h:Float,i:Int) {
+        this.size = w/6
+        this.x = (i%3)*((w/3)+size/2)
+        this.y = (i/3)*((h/3)+size/2)
+        this.i = i
+
+    }
+    fun draw(canvas:Canvas) {
+        canvas.save()
+        canvas.translate(x,y)
+        paint.color = Color.GRAY
+        canvas.drawCircle(0.0f,0.0f,size/2,paint)
+        canvas.save()
+        canvas.scale(scale,scale)
+        paint.color = Color.YELLOW
+        canvas.drawCircle(0.0f,0.0f,size/2,paint)
+        canvas.restore()
+        canvas.restore()
+    }
+    fun handleTap(x:Float,y:Float):Boolean {
+        var cond =  x>this.x-size/2 && x<this.x+size/2 && y>this.y-size/2 && y<this.y+size/2 && dir == 0.0f
+        if(cond) {
+            when(scale) {
+                0.0f->{
+                    dir = 1.0f
+                }
+                1.0f->{
+                    dir = -1.0f
+                }
+            }
+        }
+        return cond
+    }
+    fun update() {
+        scale += dir*0.2f
+        if(scale > 1.0f) {
+            scale = 0.0f
+            dir = 0.0f
+        }
+        if(scale < 0.0f) {
+            scale = 0.0f
+            dir = 0.0f
+        }
+    }
+    fun startUpdating(dir:Float) {
+        this.dir = dir
+    }
+    override fun hashCode():Int {
+        return this.i
     }
 }
